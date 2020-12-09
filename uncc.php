ca3a4bb2b4ed20742ac9fcd4c6ae7c1634124938
Hussam Khan <hussamkhan@Hussams-MacBook-Pro.local>
2020-12-08 19:39:03 -0500

    week 02 files'

---
+++ uncc.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC - Hussam Khan</title>
+</head>
+
+<body>
+     <h1>My UNCC Page</h1>
+     <hr>
+
+<?php
+    echo 9 * 13;
+?>
+
+</body>
+</html>
